1b06132f127a8d00196f225e60d3e34fa71553fb
Richard Cirone <76219710+cironera@users.noreply.github.com>
2022-08-24 13:12:51 -0700

    tweak logging v3

--- sync.py
+++ sync.py
@@ -113,7 +113,7 @@ class Sync:
         # gather issues
         for i in self.jira.fetch_issues(repo.get_key()):
             logger.info(
-                "gathering issue {issue}...".format(issue=i.key)
+                "gathering issue {issue}...".format(issue=i.id)
             )
             _, _, _, alert_key, _ = i.get_alert_info()
             if alert_key not in pairs:
